09b60859fa84ee1f2fffb831c70251f197350b46
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-10 13:01:52 -0300

    fix: header components

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,42 @@
 import { Link, useLocation } from "react-router-dom";
 import motorsShop from "../../assets/Motors shop.png";
+import {
+  Card,
+  CardHeader,
+  Flex,
+  IconButton,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
+import { ImMenu } from "react-icons/im";
 
 const Header = () => {
   let location = useLocation();
   return (
-    <>
-      <header>
-        <img src={motorsShop} alt="logo" />
-      </header>
-    </>
+    <Card width={"100vw"}>
+      <CardHeader
+        display={"flex"}
+        flexDirection={"row"}
+        justifyContent={"space-between"}
+      >
+        <Image src={motorsShop} alt="logo" />
+        <Menu>
+          <MenuButton
+            as={IconButton}
+            aria-label="Options"
+            icon={<ImMenu />}
+            variant="outline"
+          />
+          <MenuList>
+            <MenuItem>Cadastrar</MenuItem>
+            <MenuItem>Fazer Login</MenuItem>
+          </MenuList>
+        </Menu>
+      </CardHeader>
+    </Card>
   );
 };
 export default Header;
