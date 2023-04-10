48c2738d9542e6cbaa015e978997cdcbcdb6b7b3
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-10 17:13:45 -0300

    fix: login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,10 +1,14 @@
+import { Card, CardBody } from "@chakra-ui/react";
+import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 const Login = () => {
   return (
-    <>
+    <Card height={"100vh"}>
       <Header />
-    </>
+      <CardBody></CardBody>
+      <Footer />
+    </Card>
   );
 };
 
