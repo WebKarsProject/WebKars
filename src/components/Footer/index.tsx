e13bd2d99d5e0f6192176c64b0765a24eebcfc8c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-10 17:13:36 -0300

    feat: footer components

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,2 +1,25 @@
-const Footer = () => {};
+import { Button, Card, CardFooter, Image, Text } from "@chakra-ui/react";
+import motorsShop from "../../assets/Motors shop (1).png";
+import { SlArrowUp } from "react-icons/sl";
+
+const Footer = () => {
+  return (
+    <Card width={"100vw"} backgroundColor={"black"}>
+      <CardFooter
+        display={"flex"}
+        flexDirection={"row"}
+        justifyContent={"space-between"}
+        alignItems={"center"}
+      >
+        <Image src={motorsShop} alt="logo" width={"100px"} height={"30px"} />
+        <Text fontSize="sm" noOfLines={1} color="grey">
+          © 2023 - Todos os direitos reservados.
+        </Text>
+        <Button backgroundColor={"grey"}>
+          <SlArrowUp />
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+};
 export default Footer;
