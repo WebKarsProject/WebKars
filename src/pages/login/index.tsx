dd79095416a57ea004ce0a14563b7d210ce1849c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-12 14:36:19 -0300

    fix: pages login

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -17,7 +17,6 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import { loginSchema } from "../../schemas/Login/loginSchema";
 import { useContext } from "react";
 import { AuthContext } from "../../contexts/Auth/AuthContext";
-import ProductCard from "../../components/ProductCard";
 
 const Login = () => {
   const { login } = useContext(AuthContext);
@@ -27,66 +26,65 @@ const Login = () => {
     formState: { errors },
   } = useForm<IReqLogin>({ resolver: yupResolver(loginSchema) });
   return (
-    <ProductCard />
-    // <Card height={'100vh'}>
-    //   <Header />
-    //   <CardBody
-    //     display={"flex"}
-    //     flexDirection={"column"}
-    //     alignItems={"center"}
-    //     justifyContent={"center"}
-    //     backgroundColor={"grey_scale.grey8"}
-    //     height={"100vh"}
-    //   >
-    //     <FormControl
-    //       display="flex"
-    //       flexDirection={"column"}
-    //       gap={"0.3rem"}
-    //       width={"98%"}
-    //       maxWidth={"500px"}
-    //       borderRadius={"15px"}
-    //       padding={"2rem"}
-    //       backgroundColor={"white"}
-    //       height={"max-content"}
-    //       onSubmit={handleSubmit(login)}
-    //     >
-    //       <Heading fontSize={"1.5rem"}>Login</Heading>
-    //       <FormLabel fontSize={"1rem"}>Usuario</FormLabel>
-    //       <Input
-    //         type="email"
-    //         placeholder="Digite aqui seu email"
-    //         id="email"
-    //         {...register("email")}
-    //       />
-    //       <Text color={"feedback.alert1"}>{errors.email?.message}</Text>
-    //       <FormLabel fontSize={"1rem"}>Senha</FormLabel>
-    //       <Input
-    //         type="password"
-    //         id="password"
-    //         placeholder="Digite aqui sua senha"
-    //         {...register("password")}
-    //       />
-    //       <Text color={"feedback.alert1"}>{errors.password?.message}</Text>
-    //       <Link
-    //         textAlign={"end"}
-    //         fontSize={"1rem"}
-    //         _hover={{ textDecoration: "none" }}
-    //       >
-    //         esqueci minha senha
-    //       </Link>
-    //       <Button variant={'brand1'} type="submit">
-    //         Entrar
-    //       </Button>
-    //       <Text textAlign={'center'} fontSize={'1rem'}>
-    //         Ainda não possui conta?
-    //       </Text>
-    //       <Link href="/register" variant={'outline1'}>
-    //         Cadastrar
-    //       </Link>
-    //     </FormControl>
-    //   </CardBody>
-    //   <Footer />
-    // </Card>
+    <Card height={"100vh"}>
+      <Header />
+      <CardBody
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        backgroundColor={"grey_scale.grey8"}
+        height={"100vh"}
+      >
+        <FormControl
+          display="flex"
+          flexDirection={"column"}
+          gap={"0.3rem"}
+          width={"98%"}
+          maxWidth={"500px"}
+          borderRadius={"15px"}
+          padding={"2rem"}
+          backgroundColor={"white"}
+          height={"max-content"}
+          onSubmit={handleSubmit(login)}
+        >
+          <Heading fontSize={"1.5rem"}>Login</Heading>
+          <FormLabel fontSize={"1rem"}>Usuario</FormLabel>
+          <Input
+            type="email"
+            placeholder="Digite aqui seu email"
+            id="email"
+            {...register("email")}
+          />
+          <Text color={"feedback.alert1"}>{errors.email?.message}</Text>
+          <FormLabel fontSize={"1rem"}>Senha</FormLabel>
+          <Input
+            type="password"
+            id="password"
+            placeholder="Digite aqui sua senha"
+            {...register("password")}
+          />
+          <Text color={"feedback.alert1"}>{errors.password?.message}</Text>
+          <Link
+            textAlign={"end"}
+            fontSize={"1rem"}
+            _hover={{ textDecoration: "none" }}
+          >
+            esqueci minha senha
+          </Link>
+          <Button variant={"brand1"} type="submit">
+            Entrar
+          </Button>
+          <Text textAlign={"center"} fontSize={"1rem"}>
+            Ainda não possui conta?
+          </Text>
+          <Link href="/register" variant={"outline1"}>
+            Cadastrar
+          </Link>
+        </FormControl>
+      </CardBody>
+      <Footer />
+    </Card>
   );
 };
 
