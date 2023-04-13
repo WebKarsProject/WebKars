dbb363cc1869e48dd8be60e7a5858997d5f914a8
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-13 12:32:55 -0300

    fix: home page

--- src/pages/homePage/index.tsx
+++ src/pages/homePage/index.tsx
@@ -1,12 +1,7 @@
 import { Box, Flex, Heading, Text } from "@chakra-ui/layout";
 import Footer from "../../components/Footer";
 import Header from "../../components/Header";
-import {
-  Image,
-  useDisclosure,
-  useMediaQuery,
-  useTheme,
-} from "@chakra-ui/react";
+import { Image, useMediaQuery, useTheme } from "@chakra-ui/react";
 import { transparentize } from "@chakra-ui/theme-tools";
 import photo_home from "../../assets/Photo_homepage.png";
 import ProductCard from "../../components/ProductCard";
@@ -20,89 +15,87 @@ const Homepage = () => {
   return (
     <>
       <Header />
-      <Box>
-        <Box width={"100%"} maxWidth={"none"} height={"450px"}>
+      <Box width={"100%"} maxWidth={"none"} height={"450px"}>
+        <Flex
+          position="relative"
+          alignItems="center"
+          justifyContent="center"
+          height="100%"
+          width="100%"
+        >
           <Flex
             position="relative"
+            height="400px"
+            width="100%"
             alignItems="center"
             justifyContent="center"
-            height="100%"
-            width="100%"
           >
-            <Flex
-              position="relative"
+            <Image
+              src={photo_home}
+              alt="Motor Shop"
+              objectFit="cover"
+              width="950px"
               height="400px"
-              width="100%"
-              alignItems="center"
-              justifyContent="center"
+            ></Image>
+            <Box
+              position="absolute"
+              top="0"
+              left="0"
+              right="0"
+              bottom="0"
+              background={`linear-gradient(180deg, ${transparentize(
+                "white",
+                0.7
+              )(theme)} 0%, ${transparentize("black", 0.8)(theme)} 40%)`}
+              zIndex="1"
             >
-              <Image
-                src={photo_home}
-                alt="Motor Shop"
-                objectFit="cover"
-                width="950px"
-                height="400px"
-              ></Image>
               <Box
                 position="absolute"
-                top="0"
-                left="0"
-                right="0"
-                bottom="0"
-                background={`linear-gradient(180deg, ${transparentize(
-                  "white",
-                  0.7
-                )(theme)} 0%, ${transparentize("black", 0.8)(theme)} 40%)`}
-                zIndex="1"
+                top={"50%"}
+                left={"50%"}
+                transform={"translate(-50%, -50%)"}
+                color={"white"}
+                textAlign={"center"}
+                zIndex={"2"}
               >
-                <Box
-                  position="absolute"
-                  top={"50%"}
-                  left={"50%"}
-                  transform={"translate(-50%, -50%)"}
-                  color={"white"}
-                  textAlign={"center"}
-                  zIndex={"2"}
-                >
-                  <Heading fontSize={"48px"}>Motor Shop</Heading>
-                  <Text fontSize={"25px"}>
-                    A melhor plataforma de anúncios de carros do país
-                  </Text>
-                </Box>
+                <Heading fontSize={"48px"}>Motor Shop</Heading>
+                <Text fontSize={"25px"}>
+                  A melhor plataforma de anúncios de carros do país
+                </Text>
               </Box>
-            </Flex>
+            </Box>
           </Flex>
-        </Box>
-        <Box maxWidth={"none"} display={"flex"} gap={"30px"} padding={"30px"}>
-          <Flex width={"100%"}>
-            {/* Aqui é o filtro do decktop */}
-            {isLarger && <FilterProduct />}
-            <Flex
-              height={"100%"}
-              width={"70%"}
-              maxWidth={"1300px"}
-              bg={""}
-              flexDirection={"column"}
-              gap={"1rem"}
-            >
-              {" "}
-              <Box
-                height={"fit-content"}
-                display={"flex"}
-                gap={"1rem"}
-                flexWrap={"wrap"}
-                h={"100%"}
-              >
-                {/* aonde fica os produtos */}
-                <ProductCard />
-              </Box>
-              {!isLarger && <FilterProduct />}
-              {/* Paginação */}
-              <Pagination />
-            </Flex>
-          </Flex>
-        </Box>
+        </Flex>
       </Box>
+      <Flex w={"100%"} h={"100%"} justifyContent={"center"}>
+        {/* Aqui é o filtro do decktop */}
+        {isLarger && <FilterProduct />}
+        <Flex
+          height={"100%"}
+          w={"70%"}
+          flexDirection={"column"}
+          justifyContent={"space-between"}
+          gap={"1rem"}
+        >
+          <Box
+            height={"fit-content"}
+            display={"flex"}
+            gap={"1rem"}
+            flexWrap={"wrap"}
+            h={"100%"}
+          >
+            {/* aonde fica os produtos */}
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+            <ProductCard />
+          </Box>
+          {!isLarger && <FilterProduct />}
+          {/* Paginação */}
+          <Pagination />
+        </Flex>
+      </Flex>
       <Footer />
     </>
   );
