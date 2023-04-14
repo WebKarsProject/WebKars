e6bd46df9bc17aae9f90ee3172b85d21682a7757
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 17:41:49 -0300

    style: components addComments BtnComments

--- src/components/AddComments/BtnComments/index.tsx
+++ src/components/AddComments/BtnComments/index.tsx
@@ -1,4 +1,4 @@
-import { Button } from '@chakra-ui/react';
+import { Button } from "@chakra-ui/react";
 interface IBtnCommentsProps {
   text: string;
   isLarger: boolean;
@@ -8,21 +8,11 @@ const BtnComments = ({ text, isLarger }: IBtnCommentsProps) => {
   console.log(isLarger);
   return (
     <Button
-      h="24px"
-      p="0px 12px"
-      borderRadius="24px"
-      fontWeight="500"
-      fontSize="12px"
-      lineHeight="24px"
-      color="grey_scale.grey3"
-      bg="grey_scale.grey7"
-      mb={isLarger ? '0' : '16px'}
-      border="1.5px solid #E9ECEF"
-      _hover={{
-        border: '1.5px solid #5126EA',
-        color: `#FFFFFF`,
-        bg: `#5126EA`,
-      }}
+      variant={"brand2"}
+      h={"24px"}
+      fontWeight={"500"}
+      fontSize={"12px"}
+      lineHeight={"24px"}
     >
       {text}
     </Button>
