1acd4793b9d92cc924fb49b9b08a182ab2ed4341
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 17:38:21 -0300

    style: components Description

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -1,16 +1,26 @@
-import { Box, Text } from '@chakra-ui/react'
+import { Box, Text } from "@chakra-ui/react";
 
 const Description = () => {
   return (
-    <Box w={['100%', '80%', '100%', '60%']} p="36px 44px" mt={['20px', '20px', '20px', '-110px']} mb={'16px'} bg="grey_scale.grey10" borderRadius={'4px'}>
-      <Text fontWeight="600" fontSize="20px" lineHeight="25px" color="grey_scale.grey1" mb="32px">
+    <Box
+      w={["100%", "80%", "100%", "60%"]}
+      p={"36px 44px"}
+      mt={["20px", "20px", "20px", "-110px"]}
+      mb={"16px"}
+      bg={"grey_scale.grey10"}
+      borderRadius={"4px"}
+    >
+      <Text variant={"Heading-6-600"} mb={"32px"}>
         Descrição
       </Text>
-      <Text fontWeight="400" fontSize="16px" lineHeight="28px">
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      <Text variant={"body-1-400"} textAlign={"justify"}>
+        Lorem Ipsum is simply dummy text of the printing and typesetting
+        industry. Lorem Ipsum has been the industry's standard dummy text ever
+        since the 1500s, when an unknown printer took a galley of type and
+        scrambled it to make a type specimen book.
       </Text>
     </Box>
-  )
-}
+  );
+};
 
-export default Description
+export default Description;
