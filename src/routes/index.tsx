32ff68112b20885b30a050c0e0e0cae39da694aa
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 00:14:39 -0300

    feat: modified routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,22 +1,13 @@
-import { Route, Routes } from 'react-router-dom';
-import Login from '../pages/login';
-import Homepage from '../pages/homePage';
-import ProductPage from '../pages/ProductPage'
+import { Route, Routes } from "react-router-dom";
+import Login from "../pages/login";
+import Homepage from "../pages/homePage";
+import ProductPage from "../pages/ProductPage";
 
 const Routed = () => (
   <Routes>
-    <Route
-      path="/"
-      element={<Login />}
-    />
-    <Route
-      path="/homepage"
-      element={<Homepage />}
-    />
-    <Route 
-      path="/product" 
-      element={<ProductPage />} 
-    />
+    <Route path="/" element={<Login />} />
+    <Route path="/homepage" element={<Homepage />} />
+    <Route path="/product" element={<ProductPage />} />
   </Routes>
-)
-export default Routed
+);
+export default Routed;
