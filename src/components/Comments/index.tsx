5d0ceba9e47f875488eb7f21e0fc39615d2ce701
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 17:40:53 -0300

    style: components Comments

--- src/components/Comments/index.tsx
+++ src/components/Comments/index.tsx
@@ -1,17 +1,25 @@
-import { Text, UnorderedList } from '@chakra-ui/react'
-import CardLi from './Cards'
-import AddComments from '../AddComments'
+import { Text, UnorderedList } from "@chakra-ui/react";
+import CardLi from "./Cards";
 
 const Comments = () => {
   return (
-    <UnorderedList display="flex" borderRadius={'4px'} flexDirection="column" m={0} w={['100%', '80%', '100%', '60%']} mb="33px" p="36px 44px" bg="grey_scale.grey10">
-      <Text color="grey_scale.grey1" mb="24px">
+    <UnorderedList
+      display={"flex"}
+      borderRadius={"4px"}
+      flexDirection={"column"}
+      m={0}
+      w={["100%", "80%", "100%", "60%"]}
+      mb={"33px"}
+      p={"36px 44px"}
+      bg={"grey_scale.grey10"}
+    >
+      <Text variant={"Heading-6-600"} mb={"24px"}>
         Comentários
       </Text>
       <CardLi />
       <CardLi />
     </UnorderedList>
-  )
-}
+  );
+};
 
-export default Comments
+export default Comments;
