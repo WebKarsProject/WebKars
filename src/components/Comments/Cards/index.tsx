703b6c77bb1cacc1cac15f1ec4713f530694ca82
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 17:38:33 -0300

    style: components Cards

--- src/components/Comments/Cards/index.tsx
+++ src/components/Comments/Cards/index.tsx
@@ -1,40 +1,28 @@
-import { Avatar, Box, Icon, ListItem, Text } from '@chakra-ui/react';
+import { Avatar, Box, Icon, ListItem, Text } from "@chakra-ui/react";
 
 const CardLi = () => {
   return (
     <ListItem
-      listStyleType="none"
-      display="flex"
-      flexDirection="column"
-      gap="8px"
-      mb="44px"
+      listStyleType={"none"}
+      display={"flex"}
+      flexDirection={"column"}
+      gap={"8px"}
+      mb={"44px"}
     >
-      <Box display="flex" alignItems="center" gap="8px">
-        <Avatar name="J L" />
-        <Text
-          fontWeight="500"
-          fontSize="14px"
-          lineHeight="24px"
-          color="grey_scale.grey1"
-        >
-          Júlia Lima
-        </Text>
-        <Icon viewBox="0 0 300 200" color="grey_scale.grey3">
+      <Box display={"flex"} alignItems={"center"} gap={"8px"}>
+        <Avatar size={"sm"} name={"J L"} />
+        <Text variant={"body-2-500"}>Júlia Lima</Text>
+        <Icon viewBox={"0 0 500 120"} color={"grey_scale.grey3"}>
           <path
-            fill="currentColor"
-            d="M 100, 100 m -75, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
+            fill={"currentColor"}
+            d={"M 100, 100 m 30, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"}
           />
         </Icon>
-        <Text
-          fontWeight="400"
-          fontSize="12px"
-          lineHeight="24px"
-          color="grey_scale.grey3"
-        >
+        <Text variant={"body-2-400"} fontSize={"12px"}>
           há 3 dias
         </Text>
       </Box>
-      <Text fontWeight="400" fontSize="14px" lineHeight="24px">
+      <Text variant={"body-2-400"} textAlign={"justify"}>
         Lorem Ipsum is simply dummy text of the printing and typesetting
         industry. Lorem Ipsum has been the industry's standard dummy text ever
         since the 1500s, when an unknown printer took a galley of type and
