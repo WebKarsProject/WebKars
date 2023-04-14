73bae49cab402d2c811d33a16eb9493e4e21a964
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 17:41:05 -0300

    style: components Banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -4,35 +4,40 @@ import photo_home from "../../assets/Photo_homepage.png";
 const Banner = () => {
   return (
     <Flex
-      as="section"
-      alignItems="center"
-      justifyContent="center"
-      flexDir="column"
-      width="100%"
-      height="450px"
+      as={"section"}
+      alignItems={"center"}
+      justifyContent={"center"}
+      flexDir={"column"}
+      width={"100%"}
+      height={"450px"}
     >
-      <Image src={photo_home} h="100%" alt="Motor Shop" objectFit="cover" />
+      <Image
+        src={photo_home}
+        h={"100%"}
+        alt={"Motor Shop"}
+        objectFit={"cover"}
+      />
       <Flex
-        w="100%"
-        h="450px"
-        alignItems="center"
-        justifyContent="center"
-        flexDir="column"
-        bgGradient="linear(to bottom, #00000029 0%, #000000 100%)"
-        position="absolute"
-        p="0 30px"
+        w={"100%"}
+        h={"450px"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        flexDir={"column"}
+        bgGradient={"linear(to bottom, #00000029 0%, #000000 100%)"}
+        position={"absolute"}
+        p={"0 30px"}
       >
         <Text
-          variant="Heading-2-600"
-          color="grey_scale.grey10"
-          textAlign="center"
+          variant={"Heading-2-600"}
+          color={"grey_scale.grey10"}
+          textAlign={"center"}
         >
           Motor Shop
         </Text>
         <Text
-          variant="Heading-2-600"
-          color="grey_scale.grey10"
-          textAlign="center"
+          variant={"Heading-2-600"}
+          color={"grey_scale.grey10"}
+          textAlign={"center"}
         >
           A melhor plataforma de anúncios de carros do país
         </Text>
