9792288fdd1bce364b73c1a06f1a59e917b1c1ab
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-14 00:17:21 -0300

    feat: add components Banner

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,35 @@
+import { Flex, Text, Image } from "@chakra-ui/react";
+import photo_home from "../../assets/Photo_homepage.png";
+
+const Banner = () => {
+  return (
+    <Flex
+      as="section"
+      alignItems="center"
+      justifyContent="center"
+      flexDir="column"
+      width="100%"
+      height="450px"
+    >
+      <Image src={photo_home} h="100%" alt="Motor Shop" objectFit="cover" />
+      <Flex
+        w="100%"
+        h="450px"
+        alignItems="center"
+        justifyContent="center"
+        flexDir="column"
+        bgGradient="linear(to bottom, #00000029 0%, #000000 100%)"
+        position="absolute"
+      >
+        <Text variant="Heading-2-600" color="grey_scale.grey10">
+          Motor Shop
+        </Text>
+        <Text variant="Heading-2-600" color="grey_scale.grey10">
+          A melhor plataforma de anúncios de carros do país
+        </Text>
+      </Flex>
+    </Flex>
+  );
+};
+
+export default Banner;
