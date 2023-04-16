a4433fed79eaae020b3b21718ba5b46d4e59c63c
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-15 22:24:04 -0300

    feat: add profile

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 import Session from "../pages/Session";
 import Homepage from "../pages/homePage";
 import ProductPage from "../pages/ProductPage";
+import ProfilePage from "../pages/ProfilePage";
 
 const Routed = () => (
   <Routes>
     <Route path="/" element={<Homepage />} />
     <Route path="/session" element={<Session />} />
     <Route path="/product" element={<ProductPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
   </Routes>
 );
 export default Routed;
