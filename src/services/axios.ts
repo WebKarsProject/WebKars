768eae432854e03a3cb090200c623046fa4c13bc
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-18 17:09:54 -0300

    feat: services kenzieKars

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,6 +1,11 @@
 import axios from "axios";
 
-export const instance = axios.create({
+export const Instance = axios.create({
   baseURL: "http://localhost:3000/",
+  timeout: 1000,
+});
+
+export const kenzieKars = axios.create({
+  baseURL: "https://kenzie-kars.herokuapp.com/",
   timeout: 1000,
 });
