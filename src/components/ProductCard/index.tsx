2d94cbddb960f9aba19c4bcac7d937673ccb846f
lfeitosaf <lucas-jpl@hotmail.com>
2023-04-18 16:04:23 -0300

    fix: productcard

--- src/components/ProductCard/index.tsx
+++ src/components/ProductCard/index.tsx
@@ -17,26 +17,16 @@ import {
 } from "@chakra-ui/react";
 import ford from "../../assets/ford.png";
 
-const imgBox: HTMLElement | null = document.getElementById("imgBox");
-const cardBox: HTMLElement | null = document.getElementById("cardBox");
-
-cardBox?.addEventListener(
-  "mouseenter",
-  () => (imgBox!.style.border = "1.5px solid #4529E6")
-);
-
-cardBox?.addEventListener("mouseleave", () => (imgBox!.style.border = "none"));
-
 const ProductCard = () => {
   return (
-    <Card maxW="300px" id="cardBox">
+    <Card maxW="300px">
       <CardBody
         alignItems={"center"}
         display={"flex"}
         flexDirection={"column"}
         paddingBottom={0}
       >
-        <Box bg={"grey_scale.grey7"} id="imgBox">
+        <Box bg={"grey_scale.grey7"}>
           <Tag
             position={"absolute"}
             color={"grey_scale.whiteFixed"}
