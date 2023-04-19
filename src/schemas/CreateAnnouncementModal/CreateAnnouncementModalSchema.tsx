821b51112c36083180e68fc162339b4020af9f6e
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-19 11:54:51 -0300

    refactor: schemas CreateAnnouncementModalSchema

--- src/schemas/CreateAnnouncementModal/CreateAnnouncementModalSchema.tsx
+++ src/schemas/CreateAnnouncementModal/CreateAnnouncementModalSchema.tsx
@@ -1,15 +1,22 @@
 import * as yup from "yup";
+import { SchemaOf } from "yup";
+import { ICreateAnnouncementModal, IImage } from "../../interface";
 
-export const CreateAnnouncementModalSchema = yup.object().shape({
-  brand: yup.string().required(),
-  model: yup.string().required(),
-  year: yup.string().required(),
-  fuel: yup.string().required(),
-  mileage: yup.string().required(),
-  color: yup.string().required(),
-  fipe: yup.number().required(),
-  price: yup.number().required(),
-  description: yup.string().notRequired(),
-  published: yup.string().required(),
+const ImageSchema: SchemaOf<IImage> = yup.object().shape({
   img: yup.string().notRequired(),
 });
+
+export const CreateAnnouncementModalSchema: SchemaOf<ICreateAnnouncementModal> =
+  yup.object().shape({
+    brand: yup.string().required(),
+    model: yup.string().required(),
+    year: yup.string().required(),
+    fuel: yup.string().required(),
+    mileage: yup.number().required(),
+    color: yup.string().required(),
+    fipe: yup.number().required(),
+    price: yup.number().required(),
+    description: yup.string().notRequired(),
+    published: yup.boolean().notRequired(),
+    img: yup.string().notRequired(),
+  });
