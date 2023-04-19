939608bd7da57af4bd1c60e799710d4469625823
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-19 09:40:01 -0300

    fix: schemas users

--- src/schemas/Users/index.tsx
+++ src/schemas/Users/index.tsx
@@ -1,14 +1,22 @@
 import * as yup from "yup";
+import { SchemaOf } from "yup";
 import { addressSchema } from "../address";
+import { IUser } from "../../interface";
 
-export const userSchema = yup.object().shape({
+export const userSchema: SchemaOf<IUser> = yup.object().shape({
   name: yup.string().required(),
   email: yup.string().required(),
   cpf: yup.string().required(),
   phone: yup.string().required(),
   password: yup.string().required(),
   birthday: yup.date().required(),
-  description: yup.string().required(),
-  buyer: yup.boolean().required(),
-  address: addressSchema,
+  description: yup.string().notRequired(),
+  buyer: yup.boolean().notRequired(),
+  confirmPassword: yup
+    .string()
+    .oneOf(
+      [yup.ref("password")],
+      "Confirmação de senha deve ser igual a senha"
+    ),
+  address: yup.array().of(addressSchema).notRequired(),
 });
