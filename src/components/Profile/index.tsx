4ace49e1a37b63909c7e9c93ef65a4faf468b132
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-19 16:33:07 -0300

    fix: components profile

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -11,13 +11,16 @@ import {
   useMediaQuery,
   Button,
   useDisclosure,
-} from '@chakra-ui/react';
-import { useLocation } from 'react-router-dom';
-import ModalUserUpdate from '../ModalUpdateUser';
-import React, { useState } from 'react';
+} from "@chakra-ui/react";
+import { useLocation } from "react-router-dom";
+import ModalUserUpdate from "../ModalUpdateUser";
+import React, { useContext, useState } from "react";
+import { AuthContext } from "../../contexts/Auth/AuthContext";
 
 const Profile = () => {
-  const [isLarger] = useMediaQuery('(max-width: 500px)');
+  const { user } = useContext(AuthContext);
+  console.log(user);
+  const [isLarger] = useMediaQuery("(max-width: 500px)");
   let location = useLocation();
   const { isOpen, onOpen, onClose } = useDisclosure();
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -33,42 +36,38 @@ const Profile = () => {
   return (
     <Menu>
       {isOpen && (
-        <ModalUserUpdate
-          isOpen={isOpen}
-          onOpen={onOpen}
-          onClose={onClose}
-        />
+        <ModalUserUpdate isOpen={isOpen} onOpen={onOpen} onClose={onClose} />
       )}
       <MenuButton
         as={IconButton}
-        aria-label={'Options'}
+        aria-label={"Options"}
         icon={
           <Stack
-            direction={'row'}
-            alignItems={'center'}
+            direction={"row"}
+            alignItems={"center"}
             margin={0}
-            justifyContent={'space-between'}
+            justifyContent={"space-between"}
           >
             <Avatar
-              name={'anuciante'}
-              src={'https://bit.ly/tioluwani-kolawole'}
+              name={`${user.name}`}
+              src={"https://bit.ly/tioluwani-kolawole"}
             ></Avatar>
-            {!isLarger && <Text variant={'body-1-400'}>Anunciante</Text>}
+            {!isLarger && <Text variant={"body-1-400"}>{user.name}</Text>}
           </Stack>
         }
-        display={'flex'}
-        alignItems={'center'}
-        background={'none'}
-        _hover={{ background: 'none' }}
-        _focus={{ background: 'none' }}
-        _active={{ background: 'none' }}
-        border={'none'}
+        display={"flex"}
+        alignItems={"center"}
+        background={"none"}
+        _hover={{ background: "none" }}
+        _focus={{ background: "none" }}
+        _active={{ background: "none" }}
+        border={"none"}
       />
       <MenuList>
         <MenuItem>
           <Button
             onClick={openModal}
-            _hover={{ textDecoration: 'none', background: 'none' }}
+            _hover={{ textDecoration: "none", background: "none" }}
             bg="none"
           >
             <ModalUserUpdate
@@ -80,15 +79,15 @@ const Profile = () => {
           </Button>
         </MenuItem>
         <MenuItem>
-          <Link _hover={{ textDecoration: 'none', background: 'none' }}>
+          <Link _hover={{ textDecoration: "none", background: "none" }}>
             Editar Endereço
           </Link>
         </MenuItem>
         <MenuItem>
-          <Link _hover={{ textDecoration: 'none' }}>Meus anuncios</Link>
+          <Link _hover={{ textDecoration: "none" }}>Meus anuncios</Link>
         </MenuItem>
         <MenuItem>
-          <Link _hover={{ textDecoration: 'none', background: 'none' }}>
+          <Link _hover={{ textDecoration: "none", background: "none" }}>
             Sair
           </Link>
         </MenuItem>
