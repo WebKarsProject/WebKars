b1456e215ccdc3b533f7c32f2c65031b8d88dd18
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-21 22:35:22 -0300

    fix: components Textarea

--- src/components/Textarea/index.tsx
+++ src/components/Textarea/index.tsx
@@ -1,26 +1,38 @@
-import { FormLabel, Textarea, Stack } from "@chakra-ui/react";
+import {
+  FormLabel,
+  Textarea,
+  FormControl,
+  FormErrorMessage,
+} from "@chakra-ui/react";
 
-const Textareas = ({ label, placeholder }: any) => {
+const Textareas = ({ id, label, placeholder, register, errors }: any) => {
   return (
-    <Stack spacing={"0px"}>
+    <FormControl isInvalid={errors && errors[id]}>
       <FormLabel fontSize={"14px"} fontWeight={"500"} lineHeight={"17px"}>
         {label}
       </FormLabel>
       <Textarea
+        id={id}
         placeholder={placeholder}
+        _placeholder={{ colo: "grey_scale.grey3" }}
         display={"flex"}
         flexDirection={"row"}
         alignItems={"flex-start"}
-        height={"48px"}
+        height={"80px"}
         padding={"8px 16px"}
         gap={"10px"}
         border={"1.5px solid #F1F3F5"}
         borderRadius={"4px"}
-        color={"#868E96"}
+        color={"grey_scale.grey0"}
         focusBorderColor={"#5126EA"}
         _hover={{ background: "#F1F3F5" }}
+        {...register(id)}
+        resize={"none"}
       />
-    </Stack>
+      <FormErrorMessage color={"feedback.alert1"}>
+        {errors && errors[id] && errors[id].message}
+      </FormErrorMessage>
+    </FormControl>
   );
 };
 
