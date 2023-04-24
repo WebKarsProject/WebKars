3482b6969bd123787bd818691b063b1e724afaa4
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-24 14:43:17 -0300

    feat: manin VehicleProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/chakra.theme";
 import { BrowserRouter } from "react-router-dom";
 import AuthProvider from "./contexts/Auth/AuthContext";
-import { AnnouncementProvider } from "./contexts/Announcement/AnnouncementContexts";
+import { VehicleProvider } from "./contexts/Vehicle/VehicleContexts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
       <ChakraProvider theme={theme}>
         <AuthProvider>
-          <AnnouncementProvider>
+          <VehicleProvider>
             <App />
-          </AnnouncementProvider>
+          </VehicleProvider>
         </AuthProvider>
       </ChakraProvider>
     </BrowserRouter>
