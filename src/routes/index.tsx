9b40200e498851ca38cbf57d988549c3b3e6d83b
Henrique Jesus <henrique.jesus128@gmail.com>
2023-04-25 14:23:18 -0300

    fix: routes add recovery

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,32 +1,19 @@
-import { Route, Routes } from 'react-router-dom';
-import Session from '../pages/Session';
-import Homepage from '../pages/homePage';
-import ProductPage from '../pages/ProductPage';
-import ProfilePage from '../pages/ProfilePage';
-import RegisterPage from '../pages/RegisterPage';
+import { Route, Routes } from "react-router-dom";
+import Session from "../pages/Session";
+import Homepage from "../pages/homePage";
+import ProductPage from "../pages/ProductPage";
+import ProfilePage from "../pages/ProfilePage";
+import RegisterPage from "../pages/RegisterPage";
+import RecoveryPassword from "../pages/RecoveryPassword";
 
 const Routed = () => (
   <Routes>
-    <Route
-      path="/"
-      element={<Homepage />}
-    />
-    <Route
-      path="/session"
-      element={<Session />}
-    />
-    <Route
-      path="/product/:id"
-      element={<ProductPage />}
-    />
-    <Route
-      path="/profile"
-      element={<ProfilePage />}
-    />
-    <Route
-      path="/register"
-      element={<RegisterPage />}
-    />
+    <Route path="/" element={<Homepage />} />
+    <Route path="/session" element={<Session />} />
+    <Route path="/product/:id" element={<ProductPage />} />
+    <Route path="/profile" element={<ProfilePage />} />
+    <Route path="/register" element={<RegisterPage />} />
+    <Route path="/recovery" element={<RecoveryPassword />} />
   </Routes>
 );
 export default Routed;
