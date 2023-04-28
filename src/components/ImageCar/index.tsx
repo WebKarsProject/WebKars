86cfabc41f6b1f257ae7679ecd7b6427386f4219
JunielsonDiniz <juniosantos_@hotmail.com>
2023-04-28 15:03:31 -0300

    fix: components ImageCar

--- src/components/ImageCar/index.tsx
+++ src/components/ImageCar/index.tsx
@@ -1,12 +1,20 @@
-import { Box, Image } from '@chakra-ui/react'
-import car from '../../assets/EXTERIOR-frontSidePilotNear-1653845164710-removebg-preview 1.png'
+import { Box, Image } from "@chakra-ui/react";
+import { ICarImages } from "../../interface";
 
-const ImageCar = ({ onOpen }: any) => {
+const ImageCar = ({ image, modalCarImg }: ICarImages) => {
   return (
-    <Box onClick={onOpen} display={'flex'} borderRadius={'4px'} justifyContent={'center'} alignItems={'center'} bg="grey_scale.grey7" boxSize={'24'}>
-      <Image src={car} />
+    <Box
+      onClick={() => modalCarImg(image)}
+      display={"flex"}
+      borderRadius={"4px"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      bg="grey_scale.grey7"
+      boxSize={"24"}
+    >
+      <Image src={image.img_url} objectFit={"cover"} />
     </Box>
-  )
-}
+  );
+};
 
-export default ImageCar
+export default ImageCar;
