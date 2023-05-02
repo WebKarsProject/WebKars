72c1c4e9b273f76d2a142bd16dae038621267d3c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-02 14:08:34 -0300

    fix: component loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,31 @@
+import { Box, Spinner, Text } from "@chakra-ui/react";
+import Header from "../Header";
+import Footer from "../Footer";
+
+const Loading = () => {
+  return (
+    <>
+      <Header />
+      <Box
+        height="calc(100vh - 80px - 80px)"
+        display={"flex"}
+        flexDirection={"column"}
+        alignItems={"center"}
+        justifyContent={"center"}
+        gap={"1rem"}
+      >
+        <Spinner
+          thickness="5px"
+          speed="0.65s"
+          emptyColor="gray.200"
+          color="blue.500"
+          h={"10rem"}
+          w={"10rem"}
+        />
+        <Text variant={"Heading-1-700"}>Carrregando</Text>
+      </Box>
+      <Footer />
+    </>
+  );
+};
+export default Loading;
