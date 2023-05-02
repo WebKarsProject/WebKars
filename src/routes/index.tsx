01f487f25555fb8e808fe439b429771ae94609c7
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-02 14:08:48 -0300

    fix: route loading

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,6 +6,7 @@ import ProfilePage from "../pages/ProfilePage";
 import RegisterPage from "../pages/RegisterPage";
 import RecoveryPassword from "../pages/RecoveryPassword";
 import NotFound from "../pages/NotFound";
+import Loading from "../components/Loading";
 
 const Routed = () => (
   <Routes>
@@ -15,7 +16,8 @@ const Routed = () => (
     <Route path="/profile/:id" element={<ProfilePage />} />
     <Route path="/register" element={<RegisterPage />} />
     <Route path="users/resetPassword/:token" element={<RecoveryPassword />} />
-    <Route path="/notFound" element={<NotFound />}></Route>
+    <Route path="/notFound" element={<NotFound />} />
+    <Route path="/loading" element={<Loading />} />
   </Routes>
 );
 export default Routed;
