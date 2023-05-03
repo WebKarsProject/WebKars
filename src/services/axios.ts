7b7e6bcd43296db2811ccef145400033f6b24a46
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-03 15:38:31 -0300

    fix: add url render

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const Instance = axios.create({
   baseURL: "http://localhost:3000/",
+  // baseURL: "https://web-kars-api.onrender.com",
   timeout: 5000,
 });
 
