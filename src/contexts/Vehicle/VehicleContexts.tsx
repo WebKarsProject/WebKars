8f75a0c410291b5942ebeebc911754f0f7b92404
Alexson Pereira <alexspnpe@gmail.com>
2023-05-03 10:45:01 -0300

    fix: error return api vehicles

--- src/contexts/Vehicle/VehicleContexts.tsx
+++ src/contexts/Vehicle/VehicleContexts.tsx
@@ -1,91 +1,82 @@
-import { createContext, useState, useContext, useEffect } from "react";
-import {
-  IAxiosData,
-  IProviderProps,
-  IUrlImg,
-  IVehicleBody,
-  IVehicleContext,
-  IVehiclePost,
-} from "../../interface";
-import { Instance } from "../../services/axios";
-import axios from "axios";
-import { AuthContext } from "../Auth/AuthContext";
-import foto from "../../assets/naoDisponivel.jpg";
-import { useDisclosure } from "@chakra-ui/react";
-import { toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
+import { createContext, useState, useContext, useEffect } from 'react'
+import { IAxiosData, IProviderProps, IUrlImg, IVehicleBody, IVehicleContext, IVehiclePost } from '../../interface'
+import { Instance } from '../../services/axios'
+import axios from 'axios'
+import { AuthContext } from '../Auth/AuthContext'
+import foto from '../../assets/naoDisponivel.jpg'
+import { useDisclosure } from '@chakra-ui/react'
+import { toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
-export const VehicleContext = createContext<IVehicleContext>(
-  {} as IVehicleContext
-);
+export const VehicleContext = createContext<IVehicleContext>({} as IVehicleContext)
 
 const VehicleProvider = ({ children }: IProviderProps) => {
-  const [adVehicle, setAdVehicle] = useState([] as IVehiclePost[]);
-  const [allCars, setAllCars] = useState([] as IVehiclePost[]);
-  const [inputModal, setInputModal] = useState<number[]>([1]);
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  const { setLoading } = useContext(AuthContext);
+  const [adVehicle, setAdVehicle] = useState([] as IVehiclePost[])
+  const [allCars, setAllCars] = useState([] as IVehiclePost[])
+  const [inputModal, setInputModal] = useState<number[]>([1])
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { setLoading } = useContext(AuthContext)
 
   useEffect(() => {
     const getVehicle = async () => {
       try {
-        const { data } = await Instance.get<Array<IVehiclePost>>("/vehicle");
-        setAdVehicle(data);
-        setAllCars(data);
-        return data;
+        const { data } = await Instance.get('/vehicle')
+        setAdVehicle(data.pagination.vehicles)
+        setAllCars(data.pagination.vehicles)
+        return data.pagination.vehicles
       } catch (error) {
         if (axios.isAxiosError(error)) {
-          const data = error.response?.data as IAxiosData;
-          toast.error(`${data.message}❗❗`);
+          const data = error.response?.data as IAxiosData
+          toast.error(`${data.message}❗❗`)
         }
       } finally {
-        setLoading(false);
+        setLoading(false)
       }
-    };
-    getVehicle();
-  }, []);
+    }
+    getVehicle()
+  }, [])
 
   const createVehicle = async (body: IVehiclePost) => {
-    setLoading(true);
+    setLoading(true)
     try {
-      await Instance.post("/vehicle", body);
+      await Instance.post('/vehicle', body)
     } catch (error) {
       if (axios.isAxiosError(error)) {
-        const data = error.response?.data as IAxiosData;
-        toast.error(`${data.message}❗❗`);
+        const data = error.response?.data as IAxiosData
+        toast.error(`${data.message}❗❗`)
       }
     } finally {
-      setLoading(false);
+      setLoading(false)
     }
-  };
+  }
 
   const addVehicle = (body: IVehicleBody) => {
-    const newImages: IUrlImg[] = [];
+    const newImages: IUrlImg[] = []
 
     body.images.map((imgs) => {
       {
-        imgs.length !== 0 && newImages.push({ img_url: imgs });
+        imgs.length !== 0 && newImages.push({ img_url: imgs })
       }
-    });
+    })
 
     {
       newImages.length === 0 &&
         newImages.push({
-          img_url: foto,
-        });
+          img_url: foto
+        })
     }
 
-    Reflect.deleteProperty(body, "images");
+    Reflect.deleteProperty(body, 'images')
 
     const data: IVehiclePost = {
       ...body,
       images: newImages,
-      published: true,
-    };
+      published: true
+    }
 
-    createVehicle(data);
-    onClose();
-  };
+    createVehicle(data)
+    onClose()
+  }
 
   return (
     <VehicleContext.Provider
@@ -99,12 +90,12 @@ const VehicleProvider = ({ children }: IProviderProps) => {
         inputModal,
         setInputModal,
         allCars,
-        setAllCars,
+        setAllCars
       }}
     >
       {children}
     </VehicleContext.Provider>
-  );
-};
+  )
+}
 
-export default VehicleProvider;
+export default VehicleProvider
