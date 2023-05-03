dff7bebf8dcb585bc323d073f5e5f1ec8d4d7c1c
Henrique Jesus <henrique.jesus128@gmail.com>
2023-05-03 20:35:34 -0300

    fix: component pagination

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,6 +1,9 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Button, Flex, Text } from "@chakra-ui/react";
+import { VehicleContext } from "../../contexts/Vehicle/VehicleContexts";
+import { useContext } from "react";
 
 const Pagination = () => {
+  const { page, setPage, infoPage } = useContext(VehicleContext);
   return (
     <Flex
       display={"flex"}
@@ -12,13 +15,34 @@ const Pagination = () => {
       h={"66px"}
       m={"30px 0"}
     >
+      {page !== 1 ? (
+        <Text
+          onClick={() => setPage(page - 1)}
+          color={"#5126EA"}
+          fontWeight={"bold"}
+          cursor={"pointer"}
+        >
+          {`<`} Anterior
+        </Text>
+      ) : (
+        <Text>⠀⠀⠀⠀⠀⠀⠀</Text>
+      )}
       <Flex gap={"5px"} fontWeight={"bold"}>
-        <Text color={"#868E96.200"}> 1 </Text>
-        <Text color={"#868E96"}> de 2</Text>
+        <Text color={"#868E96.200"}> {page} </Text>
+        <Text color={"#868E96"}> de {infoPage.totalPages}</Text>
       </Flex>
-      <Text color={"#5126EA"} fontWeight={"bold"}>
-        Seguinte
-      </Text>
+      {infoPage.totalPages !== page ? (
+        <Text
+          onClick={() => setPage(page + 1)}
+          color={"#5126EA"}
+          fontWeight={"bold"}
+          cursor={"pointer"}
+        >
+          Seguinte {`>`}
+        </Text>
+      ) : (
+        <Text>⠀⠀⠀⠀⠀⠀⠀⠀</Text>
+      )}
     </Flex>
   );
 };
