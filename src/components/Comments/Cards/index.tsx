6a1cab6a3341c4764a828943b41201e59998e903
Alexson Pereira <alexspnpe@gmail.com>
2023-05-08 13:50:21 -0300

    fix: edit comenter

--- src/components/Comments/Cards/index.tsx
+++ src/components/Comments/Cards/index.tsx
@@ -1,78 +1,65 @@
-import { useCallback, useContext, useEffect } from "react";
-import { commentContext } from "../../../contexts/Comment/commentContext";
-import { Spinner } from "@chakra-ui/react";
-import { AuthContext } from "../../../contexts/Auth/AuthContext";
-import { formatDistance } from "date-fns";
-import { pt } from "date-fns/locale";
-import ModalEditComment from "../../ModalEditComment";
-import { Avatar, Box, Icon, ListItem, Text } from "@chakra-ui/react";
+import { useCallback, useContext, useEffect } from 'react'
+import { commentContext } from '../../../contexts/Comment/commentContext'
+import { Spinner } from '@chakra-ui/react'
+import { AuthContext } from '../../../contexts/Auth/AuthContext'
+import { formatDistance } from 'date-fns'
+import { pt } from 'date-fns/locale'
+import ModalEditComment from '../../ModalEditComment'
+import { Avatar, Box, Icon, ListItem, Text } from '@chakra-ui/react'
 
 const timeAgo = (timestamp: string) => {
-  const commentDate = new Date(timestamp);
-  const currentDate = new Date();
+  const commentDate = new Date(timestamp)
+  const currentDate = new Date()
   return formatDistance(commentDate, currentDate, {
     addSuffix: true,
-    locale: pt,
-  });
-};
+    locale: pt
+  })
+}
 
 const CardLi = ({ cars }: any) => {
-  const { getComment, comments } = useContext(commentContext);
-  const { loading, setLoading } = useContext(AuthContext);
+  const { getComment, comments } = useContext(commentContext)
+  const { loading, setLoading } = useContext(AuthContext)
 
-  const idUser = cars.id;
+  const idUser = cars.id
 
-  const specificComment = comments[idUser];
+  const specificComment = comments[idUser]
 
   const memoizedGetComment = useCallback(async () => {
-    await getComment(idUser);
-  }, [getComment, idUser]);
+    await getComment(idUser)
+  }, [getComment, idUser])
 
   useEffect(() => {
     async function fetchData() {
-      await memoizedGetComment();
+      await memoizedGetComment()
     }
-    fetchData();
-  }, []);
+    fetchData()
+  }, [])
 
   return (
-    <ListItem
-      listStyleType={"none"}
-      display={"flex"}
-      flexDirection={"column"}
-      gap={"8px"}
-      mb={"44px"}
-    >
+    <ListItem listStyleType={'none'} display={'flex'} flexDirection={'column'} gap={'8px'} mb={'44px'}>
       {loading ? (
         <Spinner />
       ) : (
         <>
-          <Box display={"flex"} alignItems={"center"} gap={"8px"}>
-            <Avatar size={"sm"} name={specificComment?.data.user?.name} />
-            <Text variant={"body-2-500"}>
-              {specificComment?.data.user?.name}
-            </Text>
-            <Icon viewBox={"0 0 500 120"} color={"grey_scale.grey3"} />
-            <Icon viewBox={"0 0 500 120"} color={"grey_scale.grey3"}>
-              <path
-                fill={"currentColor"}
-                d={
-                  "M 100, 100 m 30, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0"
-                }
-              />
+          <Box display={'flex'} alignItems={'center'} gap={'8px'}>
+            <Avatar size={'sm'} name={specificComment?.data.user?.name} />
+            <Text variant={'body-2-500'}>{specificComment?.data.user?.name}</Text>
+            <Icon viewBox={'0 0 500 120'} color={'grey_scale.grey3'} />
+            <Icon viewBox={'0 0 500 120'} color={'grey_scale.grey3'}>
+              <path fill={'currentColor'} d={'M 100, 100 m 30, 0 a 75,75 0 1,0 150,0 a 75,75 0 1,0 -150,0'} />
             </Icon>
-            <Text variant={"body-2-400"} fontSize={"12px"}>
+            <Text variant={'body-2-400'} fontSize={'12px'}>
               {timeAgo(cars.createdAt)}
             </Text>
-            <ModalEditComment />
+            <ModalEditComment idUser={specificComment?.data.user.id} />
           </Box>
-          <Text variant={"body-2-400"} textAlign={"justify"}>
+          <Text variant={'body-2-400'} textAlign={'justify'}>
             {cars.description}
           </Text>
         </>
       )}
     </ListItem>
-  );
-};
+  )
+}
 
-export default CardLi;
+export default CardLi
--- src/components/ModalEditComment/index.tsx
+++ src/components/ModalEditComment/index.tsx
@@ -1,36 +1,22 @@
-import {
-  Modal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalFooter,
-  ModalBody,
-  ModalCloseButton,
-  Button,
-  useDisclosure,
-  FormControl,
-  FormLabel,
-  Textarea,
-} from '@chakra-ui/react';
-import { IconButton } from '@chakra-ui/react';
-import { AiOutlineEdit } from 'react-icons/ai';
+import { Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, Button, useDisclosure, FormControl, FormLabel, Textarea } from '@chakra-ui/react'
+import { IconButton } from '@chakra-ui/react'
+import { useContext } from 'react'
+import { AiOutlineEdit } from 'react-icons/ai'
+import { AuthContext } from '../../contexts/Auth/AuthContext'
 
-const ModalEditComment = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure();
+interface IProps {
+  idUser: string | undefined
+}
+
+const ModalEditComment = ({ idUser }: IProps) => {
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const { user } = useContext(AuthContext)
+  console.log(idUser === user.id)
 
   return (
     <>
-      <IconButton
-        aria-label="Editar descrição"
-        icon={<AiOutlineEdit />}
-        onClick={onOpen}
-        bg={'none'}
-      />
-
-      <Modal
-        isOpen={isOpen}
-        onClose={onClose}
-      >
+      {idUser === user.id && <IconButton aria-label="Editar descrição" icon={<AiOutlineEdit />} onClick={onOpen} bg={'none'} />}
+      <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader>Editar descrição</ModalHeader>
@@ -42,30 +28,20 @@ const ModalEditComment = () => {
             </FormControl>
           </ModalBody>
           <ModalFooter>
-            <Button
-              colorScheme="blue"
-              mr={3}
-              onClick={onClose}
-            >
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
               Salvar
             </Button>
-            <Button
-              variant="ghost"
-              onClick={onClose}
-            >
+            <Button variant="ghost" onClick={onClose}>
               Excluir
             </Button>
-            <Button
-              variant="ghost"
-              onClick={onClose}
-            >
+            <Button variant="ghost" onClick={onClose}>
               Cancelar
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  );
-};
+  )
+}
 
-export default ModalEditComment;
+export default ModalEditComment
